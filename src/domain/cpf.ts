3e38d396a1793ea79523f4fd853a1d15c98b64b0
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-09-24 12:06:49 -0300

    ⚙️ refactor (file): Create consts to separete values

--- src/domain/cpf.ts
+++ src/domain/cpf.ts
@@ -1,4 +1,6 @@
 const CPF_SIZE: number = 11;
+const CPF_SIZE_NO_DIGITS: number = 9;
+const FIRST_DIGIT_POSITION: number = 10
 
 export default class Cpf {
 
@@ -25,23 +27,23 @@ export default class Cpf {
     }
 
     validateFirstDigite(cpf: string): boolean {
-        const cpfNoDigits = cpf.substring(0, 9);
-        const cpfFirstDigite = cpf.substring(9, 10);
-        return this.validateDigite(cpfNoDigits) === parseInt(cpfFirstDigite);
+        const cpfNoDigits = cpf.substring(0, CPF_SIZE_NO_DIGITS);
+        const cpfFirstDigit = cpf.substring(CPF_SIZE_NO_DIGITS, FIRST_DIGIT_POSITION);
+        return this.validateDigit(cpfNoDigits) === parseInt(cpfFirstDigit);
     }
 
     validateSecondDigite(cpf: string): boolean {
-        const cpfWithFirstDigit = cpf.substring(0, 10);
-        const cpfSecondDigite = cpf.substring(10, 11);
-        return this.validateDigite(cpfWithFirstDigit) === parseInt(cpfSecondDigite);
+        const cpfWithFirstDigit = cpf.substring(0, FIRST_DIGIT_POSITION);
+        const cpfSecondDigit = cpf.substring(FIRST_DIGIT_POSITION, CPF_SIZE);
+        return this.validateDigit(cpfWithFirstDigit) === parseInt(cpfSecondDigit);
     }
 
-    validateDigite(cpf: string): number {
+    validateDigit(cpf: string): number {
         const digitsSum = cpf.split('')
-        .map((value, index, array) => parseInt(value) * (array.length - index + 1))
-        .reduce((preview, next) => {
-                return preview + next
-        });
+            .map((value, index, array) => parseInt(value) * (array.length - index + 1))
+            .reduce((preview, next) => {
+                    return preview + next
+            });
         return (digitsSum % 11) > 1 ? CPF_SIZE - (digitsSum % CPF_SIZE) : 0;
     }
 
