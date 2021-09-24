44a2395fd2e01722b55aae0ae25845a72ec5e034
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-09-24 02:44:25 -0300

    🧪 test (file): Testing validate cpf class

---
+++ src/tests/cpf.spec.ts
@@ -0,0 +1,50 @@
+import Cpf from '@/domain/cpf';
+
+
+describe('Test cpf validations', () => {
+    
+    test('Should recive a valide size cpf', () => {
+        const cpf = new Cpf();
+        expect(cpf.sizeValidate('40819992879')).toBeTruthy();
+    });
+
+    test('should recive a not valide size cpf', () => {
+        const cpf = new Cpf();
+        expect(cpf.sizeValidate('408199928')).toBeFalsy();
+    })
+    
+    test('should not receive letters in cpf', () => {
+        const cpf = new Cpf();
+        expect(cpf.verifyLetters('408199928A')).toBeFalsy();
+    })
+
+    test('should return false when all digits are equal', () => {
+        const cpf = new Cpf();
+        expect(cpf.validateIfAllEquals('11111111111')).toBeFalsy();
+    })
+    
+    
+    test('should receive cpf with first digite valid', () => {
+        const cpf = new Cpf();
+        expect(cpf.validateFirstDigite('40819992879')).toBeTruthy();
+    });
+
+    test('should receive cpf with second digit valide', () => {
+        const cpf = new Cpf();
+        expect(cpf.validateSecondDigite('40819992879')).toBeTruthy();
+    });
+})
+
+
+describe('Test cpf', () => {
+    test('should receice a invalid cpf', () => {
+        const cpf = new Cpf();
+        expect(cpf.isValid('111.111.111-11')).toBeFalsy();
+    })
+
+    test('should receice a valid cpf', () => {
+        const cpf = new Cpf();
+        expect(cpf.isValid('935.411.347-80')).toBeTruthy();
+    })    
+})
+
