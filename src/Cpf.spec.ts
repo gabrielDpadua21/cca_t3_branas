31034e8493aadd3fa5d6b16eb778d22a78587be5
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-10-20 03:16:30 -0300

    ♻️refactor(project): refactor cpf class

---
+++ src/Cpf.spec.ts
@@ -0,0 +1,20 @@
+import Cpf from './Cpf';
+
+describe('Test Cpf', () => {
+    test('Should try validate invalid cpf', () => {
+        expect(() => new Cpf('123.456.789-99')).toThrow(new Error("Invalid cpf"));
+    });
+
+    test('Should try validate Cpf with all digits equals', () => {
+        expect(() => new Cpf('111.111.111-11')).toThrow(new Error("Invalid cpf"));
+    });
+
+    test('Should try validate Cpf bigger than 11 chars', () => {
+        expect(() => new Cpf('935.411.347-8000')).toThrow(new Error("Invalid cpf"));
+    });
+
+    test('Should try validate Cpf small than 11 chars', () => {
+        expect(() => new Cpf('935.411.3')).toThrow(new Error("Invalid cpf"));
+    });
+});
+
--- src/Cpf.ts
+++ src/Cpf.ts
@@ -1,49 +1,49 @@
-const CPF_VALID_LENGTH = 11;
-const FACTOR_FIRST_DIGIT = 10;
-const FACTOR_SECOND_DIGIT = 11;
-
 export default class Cpf {
+    CPF_VALID_LENGTH: number = 11;
+    FACTOR_FIRST_DIGIT: number = 10;
+    FACTOR_SECOND_DIGIT: number = 11;
     private cpf: string;
-
-    constructor(cpf: any) {
+    
+    constructor(cpf: string) {
+        if(!this.validate(cpf)) throw new Error("Invalid cpf");
         this.cpf = cpf;
     }
 
-    validate() {
+    validate(cpf: string) {
         if(!this.cpf) return false;
-        this.cpf = this.clean();
-        if(!this.isValidLength()) return false;
-        if(this.allDigitsEquals()) return false;
-        const firstDigit = this.calculateDigit(FACTOR_FIRST_DIGIT);
-        const secondDigit = this.calculateDigit(FACTOR_SECOND_DIGIT);
-        const verifierDigit = this.extractVerifierDigit();
+        const cleanCpf = this.clean(cpf);
+        if(!this.isValidLength(cleanCpf)) return false;
+        if(this.allDigitsEquals(cleanCpf)) return false;
+        const firstDigit = this.calculateDigit(cleanCpf, this.FACTOR_FIRST_DIGIT);
+        const secondDigit = this.calculateDigit(cleanCpf, this.FACTOR_SECOND_DIGIT);
+        const verifierDigit = this.extractVerifierDigit(cleanCpf);
         const calculatedVerifiedDigit = `${firstDigit}${secondDigit}`;
         return verifierDigit == calculatedVerifiedDigit;
     }
 
-    clean() {
-        return this.cpf.replace(/\D/g, "");
+    clean(cpf: string) {
+        return cpf.replace(/\D/g, "");
     }
 
-    isValidLength() {
-        return this.cpf.length === CPF_VALID_LENGTH;
+    isValidLength(cpf: string) {
+        return this.cpf.length === this.CPF_VALID_LENGTH;
     }
 
-    allDigitsEquals() {
+    allDigitsEquals(cpf: string) {
         const [firstDigit] = this.cpf;
         return [...this.cpf].every((digit: string) => digit === firstDigit)
     }
 
-    calculateDigit(factor: number) {
+    calculateDigit(cpf: string, factor: number) {
         let total = 0;
-        [...this.cpf].map((digit: string) => {
+        [...cpf].map((digit: string) => {
             if (factor > 1) total += parseInt(digit) * factor--;
         });
         const rest = total % 11;
         return (rest < 2) ? 0 : (11 - rest);
     }
 
-    extractVerifierDigit() {
-        return this.cpf.slice(9);
+    extractVerifierDigit(cpf: string) {
+        return cpf.slice(9);
     }
 }
