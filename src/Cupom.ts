3d4479afc1e5cde9d6cd3f0b4805e8566048a192
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-10-21 03:06:38 -0300

    ✨feat(project): create domain order, orderitem, item and cupom

---
+++ src/Cupom.ts
@@ -0,0 +1,10 @@
+
+
+export default class Cupom {
+
+    constructor (
+        readonly code: string,
+        readonly percentage: number
+    ) {}
+
+}
---
+++ src/Order.ts
@@ -0,0 +1,32 @@
+import Cpf from './Cpf';
+import Cupom from './Cupom';
+import Item from './Item';
+import OrderItem from './OrderItem';
+
+export default class Order {
+    cpf: Cpf;
+    cupom: Cupom | undefined;
+    orderItems: OrderItem[];
+
+    constructor(cpf: string) {
+        this.cpf = new Cpf(cpf);
+        this.orderItems = [];
+    }
+
+    addItem(item: Item, quantity: number) {
+        this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
+    }
+
+    addCupom(cupom: Cupom) {
+        this.cupom = cupom;
+    }
+
+    getTotal() {
+        let total = 0;
+        for (const orderItem of this.orderItems) {
+            total += orderItem.getTotal();
+        }
+        if(this.cupom) total -= total * (this.cupom.percentage / 100);
+        return total
+    }
+}
