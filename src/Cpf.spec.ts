92720cba4d2fae264e6d86eb11f4aa455a9cafbd
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-10-21 03:05:19 -0300

    🧪test(file): update unit tests cpf

--- src/Cpf.spec.ts
+++ src/Cpf.spec.ts
@@ -1,6 +1,12 @@
 import Cpf from './Cpf';
 
 describe('Test Cpf', () => {
+
+    test('Shuold validate a valid cpf', () => {
+        const cpf = new Cpf('935.411.347-80');
+        expect(cpf).toBeDefined();
+    })
+
     test('Should try validate invalid cpf', () => {
         expect(() => new Cpf('123.456.789-99')).toThrow(new Error("Invalid cpf"));
     });
