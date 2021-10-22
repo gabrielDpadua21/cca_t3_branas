70226c658d811789efe0b7446a926fcb5a54cd8f
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-10-22 03:02:38 -0300

    ✨feat(file): get freight order

--- src/Order.ts
+++ src/Order.ts
@@ -4,21 +4,29 @@ import Item from './Item';
 import OrderItem from './OrderItem';
 
 export default class Order {
-    cpf: Cpf;
-    cupom: Cupom | undefined;
-    orderItems: OrderItem[];
+    private cpf: Cpf;
+    private cupom: Cupom | undefined;
+    private orderItems: OrderItem[];
+    private freight: number
 
-    constructor(cpf: string) {
+    constructor(cpf: string, readonly issueDate: Date = new Date()) {
         this.cpf = new Cpf(cpf);
         this.orderItems = [];
+        this.freight = 0;
     }
 
     addItem(item: Item, quantity: number) {
+        this.freight = item.getFreight() * quantity;
         this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
     }
 
     addCupom(cupom: Cupom) {
+        if (cupom.isExpired(this.issueDate)) return
         this.cupom = cupom;
+    }
+
+    getFreight() {
+        return this.freight;
     }
 
     getTotal() {
